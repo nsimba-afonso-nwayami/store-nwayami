6229ce1e5b5e54adb81c8ae56f266d8242e9b315
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:50:29 +0100

    Página sobre criada

---
+++ src/pages/site/Sobre.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Sobre() {
+  return (
+    <>
+      <title> Sobre | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
