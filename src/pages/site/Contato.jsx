8820b3716462d6d0b4114c7d8ffc5d984c5ab973
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:48:53 +0100

    Página contato criada

---
+++ src/pages/site/Contato.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Contato() {
+  return (
+    <>
+      <title>Contato | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
