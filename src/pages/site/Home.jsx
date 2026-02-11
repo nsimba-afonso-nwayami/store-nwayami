0dc58bcc284e6f5809312ca04d9ac39fb1fccf1c
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:51:36 +0100

    Componente otão do whatsapp adicionado na home

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import WhatsAppButton from "../../components/WhatsAppButton";
 import Hero from "../../components/home/Hero";
 import ProdutosDestaque from "../../components/home/ProdutosDestaque";
 import Categorias from "../../components/home/Categorias";
@@ -12,11 +13,14 @@ export default function Home() {
 
       {/* Header */}
       <Header />
-      
+
       <Hero />
       <ProdutosDestaque />
       <Categorias />
       <Marcas />
+
+      {/* Botão WhatsApp fixo */}
+      <WhatsAppButton phone="244972614886" size={64} />
 
       {/* Footer */}
       <Footer />
