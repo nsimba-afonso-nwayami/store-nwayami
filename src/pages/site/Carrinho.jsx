ff4c890ec02bb08b6305a0b50393e9e387d16873
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:48:26 +0100

    Página carrinho criada

---
+++ src/pages/site/Carrinho.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Carrinho() {
+  return (
+    <>
+      <title>Carrinho | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
