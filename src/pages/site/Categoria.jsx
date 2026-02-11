debc720f1d7708ba419206e6ce834214e220ea43
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:48:39 +0100

    Página categoria criada

---
+++ src/pages/site/Categoria.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Categoria() {
+  return (
+    <>
+      <title>Categoria | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
