df5bdc44e3889d012d391220f0d62d2fb0541608
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:50:16 +0100

    Página produtos criada

---
+++ src/pages/site/Produtos.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Produtos() {
+  return (
+    <>
+      <title>Produtos | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
