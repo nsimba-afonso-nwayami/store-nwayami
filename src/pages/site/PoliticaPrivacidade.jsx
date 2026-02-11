63f5b46eb4cf1c0a23e5e8d2449707f7c1523e40
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:50:02 +0100

    Página política de privacidade criada

---
+++ src/pages/site/PoliticaPrivacidade.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function PoliticaPrivacidade() {
+  return (
+    <>
+      <title>PoliticaPrivacidade | Nwayami Store</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
