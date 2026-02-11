e60481fd428c6a63dd89f8b2f8ed5c9196bf8962
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-11 11:26:30 +0100

    Novos componentes da home adicionados

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -2,6 +2,8 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Hero from "../../components/home/Hero";
 import ProdutosDestaque from "../../components/home/ProdutosDestaque";
+import Categorias from "../../components/home/Categorias";
+import Marcas from "../../components/home/Marcas";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
       
       <Hero />
       <ProdutosDestaque />
+      <Categorias />
+      <Marcas />
 
       {/* Footer */}
       <Footer />
