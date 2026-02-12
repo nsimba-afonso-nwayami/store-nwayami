86b1c6ec85b2164c7d4085860016193318c7b0fa
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-12 10:02:02 +0100

    React hot toast configurado de forma global na aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./components/ScrollToTop";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ScrollToTop />
+      <Toaster
+        position="top-right"
+        containerStyle={{
+          zIndex: 10000,
+        }}
+      />
       <AppRoutes />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
