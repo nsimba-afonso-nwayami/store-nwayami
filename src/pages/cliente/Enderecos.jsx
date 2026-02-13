143d4280f4f1bdd0483217401d8f3908e79712dd
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-13 10:26:15 +0100

    Página endereços do cliente criado

---
+++ src/pages/cliente/Enderecos.jsx
@@ -0,0 +1,12 @@
+import ClienteLayout from "./components/ClienteLayout";
+import { Link } from "react-router-dom";
+
+export default function Enderecos() {
+  return (
+    <>
+      <title>Meus Endereços | Dashboard Cliente</title>
+
+      <ClienteLayout title="Meus Endereços"></ClienteLayout>
+    </>
+  );
+}
