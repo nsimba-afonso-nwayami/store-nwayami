650d246bc84e35fbe64793f00ca84d8959ee6d2a
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-13 10:29:28 +0100

    Rotas do dashboard do cliente adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,15 +5,23 @@ import Produtos from "../pages/site/Produtos";
 import DetalhesProduto from "../pages/site/DetalhesProduto";
 import Categoria from "../pages/site/Categoria";
 import Carrinho from "../pages/site/Carrinho";
-import  Sobre from "../pages/site/Sobre";
-import  Contato from "../pages/site/Contato";
-import  PoliticaPrivacidade from "../pages/site/PoliticaPrivacidade";
+import Sobre from "../pages/site/Sobre";
+import Contato from "../pages/site/Contato";
+import PoliticaPrivacidade from "../pages/site/PoliticaPrivacidade";
 import NotFound from "../pages/site/NotFound";
 
 //Autenticação
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import RecuperarSenha from "../pages/auth/RecuperarSenha";
+
+//dashboard cliente
+import DashboardCliente from "../pages/cliente/DashboardCliente";
+import MeusPedidos from "../pages/cliente/MeusPedidos";
+import Enderecos from "../pages/cliente/Enderecos";
+import PerfilCliente from "../pages/cliente/PerfilCliente";
+import NotificacoesCliente from "../pages/cliente/NotificacoesCliente";
+import NotFoundCliente from "../pages/cliente/NotFoundCliente";
 
 export default function AppRoutes() {
   return (
@@ -33,6 +41,16 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/criar-conta" element={<Register />} />
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+
+      {/*Rotas do dashboard cliente */}
+      <Route path="/dashboard/cliente/">
+        <Route path="" element={<DashboardCliente />} />
+        <Route path="/dashboard/cliente/pedidos" element={<MeusPedidos />} />
+        <Route path="/dashboard/cliente/enderecos" element={<Enderecos />} />
+        <Route path="/dashboard/cliente/perfil" element={<PerfilCliente />} />
+        <Route path="/dashboard/cliente/notificacoes" element={<NotificacoesCliente />} />
+        <Route path="*" element={<NotFoundCliente />} />
+      </Route>
     </Routes>
   );
 }
