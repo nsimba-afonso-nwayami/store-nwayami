c564df9ea224469ef175e46c68e97d85f037a7b0
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-13 10:27:22 +0100

    Página perfil do cliente criado

---
+++ src/pages/cliente/PerfilCliente.jsx
@@ -0,0 +1,12 @@
+import ClienteLayout from "./components/ClienteLayout";
+import { Link } from "react-router-dom";
+
+export default function PerfilCliente() {
+  return (
+    <>
+      <title>Meu Perfil | Dashboard Cliente</title>
+
+      <ClienteLayout title="Meu Perfil"></ClienteLayout>
+    </>
+  );
+}
