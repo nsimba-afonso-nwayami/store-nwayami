bc7616ffbed09b5a070a1c818aa707f4efb0a560
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-13 10:26:29 +0100

    Página meus pedidos do cliente criado

---
+++ src/pages/cliente/MeusPedidos.jsx
@@ -0,0 +1,15 @@
+import ClienteLayout from "./components/ClienteLayout";
+import { Link } from "react-router-dom";
+
+export default function MeusPedidos() {
+
+  return (
+    <>
+      <title>Meus Pedidos | Dashboard Cliente</title>
+
+      <ClienteLayout title="Meus Pedidos">
+        
+      </ClienteLayout>
+    </>
+  );
+}
