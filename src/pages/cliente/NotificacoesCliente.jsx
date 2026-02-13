82837fb1834c84f52c6a39bbf775a4514780dc96
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-13 10:27:10 +0100

    Página notificações do cliente criado

---
+++ src/pages/cliente/NotificacoesCliente.jsx
@@ -0,0 +1,12 @@
+import ClienteLayout from "./components/ClienteLayout";
+import { Link } from "react-router-dom";
+
+export default function Notificacoes() {
+  return (
+    <>
+      <title>Meu Perfil | Dashboard Cliente</title>
+
+      <ClienteLayout title="Notificações"></ClienteLayout>
+    </>
+  );
+}
