ef34c1b439cd91b9cb960530a8a9d6d515901738
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-20 09:42:48 +0100

    Novo componente de produtos adicionado na home

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/home/Hero";
 import ProdutosDestaque from "../../components/home/ProdutosDestaque";
 import Categorias from "../../components/home/Categorias";
 import Marcas from "../../components/home/Marcas";
+import OutrosProdutos from "../../components/home/OutrosProdutos";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <ProdutosDestaque />
       <Categorias />
       <Marcas />
+      <OutrosProdutos />
 
       {/* Botão WhatsApp fixo */}
       <WhatsAppButton phone="244972614886" size={64} />
