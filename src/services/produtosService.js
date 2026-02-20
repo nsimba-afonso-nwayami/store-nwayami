7c819f8b267a73512d5408e8a012b5fe14008148
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-20 16:21:16 +0100

    Nova função para buscar produtos adicionada no service de produtos

--- src/services/produtosService.js
+++ src/services/produtosService.js
@@ -15,3 +15,21 @@ export async function buscarProdutoPorId(id) {
   const response = await api.get(`produtos/${id}/`);
   return response.data;
 }
+
+/**
+ * Buscar produto por slug (nome)
+ */
+export async function buscarProdutoPorSlug(slug) {
+  const response = await api.get("produtos/");
+  const produtos = response.data;
+
+  const produto = produtos.find(
+    (p) =>
+      p.descricao
+        .toLowerCase()
+        .replaceAll(" ", "-")
+        .replaceAll("/", "") === slug
+  );
+
+  return produto || null;
+}
